c10a2224d061bf91a9bc6f262b98f5270d781fad
Jim Martens <jim1@live.de>
2012-12-01 22:13:24 +0100

    Added the first steps of a 'Dame' game.

--- Main.java
+++ Main.java
@@ -8,23 +8,23 @@
  */
 public class Main
 {
-	// Enth‰lt ein Main Objekt.
+	// Enth√§lt ein Main Objekt.
 	private static Main _instance;
 	
-	// Enth‰lt ein Damebrett.
-	private final Damebrett _damebrett;
+	// Enth√§lt ein Damebrett.
+	private final  Damebrett _damebrett;
 	
-	// Enth‰lt die Kantenl‰nge.
-	private final int _kantenlaenge;
+	// Enth√§lt die Kantenl√§nge.
+	private final  int _kantenlaenge;
 	
-	// Enth‰lt die x Position.
-	private final int _xPosition;
+	// Enth√§lt die x Position.
+	private final  int _xPosition;
 	
-	// Enth‰lt die y Position.
-	private final int _yPosition;
+	// Enth√§lt die y Position.
+	private final  int _yPosition;
 	
 	/**
-	 * Gibt ein Objekt von Main zur¸ck.
+	 * Gibt ein Objekt von Main zur√ºck.
 	 * @return ein Main Objekt
 	 */
 	public static Main getInstance()
@@ -39,11 +39,11 @@ public class Main
 	/**
 	 * Initialisiert ein neues Main Objekt.
 	 */
-	private Main()
+	public Main()
 	{
-		_xPosition = 50;
-		_yPosition = 50;
-		_kantenlaenge = 30;
+		_xPosition = 100;
+		_yPosition = 100;
+		_kantenlaenge = 40;
 		_damebrett = new Damebrett(_kantenlaenge, _xPosition, _yPosition);
 	}
 	
@@ -52,7 +52,114 @@ public class Main
 	 */
 	public void start()
 	{
+		Figurenspeicher speicher = _damebrett.gibSpeicher();
+		Damefigur figur = speicher.gibFigur(2, 1);
 		
+		
+		// Zug (2,1) nach (3,0)
+		TurtleWelt.warte(4000);
+		
+		figur.setzeGeschwindigkeit(10);
+		figur.loesche();
+		
+		figur = speicher.gibFigur(3, 0);
+		figur.setzeFarbe("weiss");
+		figur.erneutZeichnen();
+		
+		// Zug (5,6) nach (4,7)
+		figur = speicher.gibFigur(5, 6);
+		TurtleWelt.warte(4000);
+		figur.loesche();
+		
+		figur = speicher.gibFigur(4, 7);
+		figur.setzeFarbe("rot");
+		figur.erneutZeichnen();
+		
+		// Zug (2,3) nach (3,4)
+		figur = speicher.gibFigur(2, 3);
+		TurtleWelt.warte(4000);
+		figur.loesche();
+		
+		figur = speicher.gibFigur(3, 4);
+		figur.setzeFarbe("weiss");
+		figur.erneutZeichnen();
+		
+		// Zug (5,0) nach (4,1)
+		figur = speicher.gibFigur(5, 0);
+		TurtleWelt.warte(4000);
+		figur.loesche();
+		
+		figur = speicher.gibFigur(4, 1);
+		figur.setzeFarbe("rot");
+		figur.erneutZeichnen();
+		
+		// Zug (1,4) nach (2,3)
+		figur = speicher.gibFigur(1, 4);
+		TurtleWelt.warte(4000);
+		figur.loesche();
+		
+		figur = speicher.gibFigur(2, 3);
+		figur.setzeFarbe("weiss");
+		figur.erneutZeichnen();
+		
+		// Zug (5,4) nach (4,5)
+		figur = speicher.gibFigur(5, 4);
+		TurtleWelt.warte(4000);
+		figur.loesche();
+				
+		figur = speicher.gibFigur(4, 5);
+		figur.setzeFarbe("rot");
+		figur.erneutZeichnen();
+		
+		// Zug (3,4) nach (5,6)
+		figur = speicher.gibFigur(3, 4);
+		TurtleWelt.warte(4000);
+		figur.loesche();
+				
+		figur = speicher.gibFigur(5, 6);
+		figur.setzeFarbe("weiss");
+		figur.erneutZeichnen();
+		
+		figur = speicher.gibFigur(4, 5);
+		figur.loesche();
+		
+		// Zug (6,7) nach (4,5)
+		figur = speicher.gibFigur(6, 7);
+		TurtleWelt.warte(4000);
+		figur.loesche();
+				
+		figur = speicher.gibFigur(4, 5);
+		figur.setzeFarbe("rot");
+		figur.erneutZeichnen();
+		
+		figur = speicher.gibFigur(5, 6);
+		figur.loesche();
+		
+		// Zug (2,7) nach (3,6)
+		figur = speicher.gibFigur(2, 7);
+		TurtleWelt.warte(4000);
+		figur.loesche();
+		
+		figur = speicher.gibFigur(3, 6);
+		figur.setzeFarbe("weiss");
+		figur.erneutZeichnen();
+		
+		// Zug (4,5) nach (2,7)
+		figur = speicher.gibFigur(4, 5);
+		TurtleWelt.warte(4000);
+		figur.loesche();
+						
+		figur = speicher.gibFigur(2, 7);
+		figur.setzeFarbe("rot");
+		figur.erneutZeichnen();
+				
+		figur = speicher.gibFigur(3, 6);
+		figur.loesche();
+		TurtleWelt.warte(4000);
+		
+		// das wars auch schon
+		TurtleWelt.globaleWelt.loescheAlleSpuren();
+		// @todo Text schreiben
 	}
 	
 	/**
